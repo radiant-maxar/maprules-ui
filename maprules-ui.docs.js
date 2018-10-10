472a6153679027932f03ab08628908e135c6e43e
Clarisse Abalos <samantha.abalos@digitalglobe.com>
2018-10-10 15:56:43 -0400

    add api docs

---
+++ maprules-ui.docs.js
@@ -0,0 +1,12 @@
+/**
+ * @api {get} /:id Edit MapRules config
+ * @apiName EditMapRule
+ * @apiGroup EditMapRule
+ * 
+ * @apiParam {String} id MapRules configuration id
+ * 
+ * @apiVersion 0.0.1
+ * 
+ * @apiExample {curl} Example Usage
+ *      curl http://localhost:4200/f343b37a-a2f5-4ee7-bcfd-2710af86c6ba
+ *
