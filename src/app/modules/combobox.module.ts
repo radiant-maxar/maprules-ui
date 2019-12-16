e62fc43d9e0cd48f4e79a8ac907bc202f4d68e9e
Max Grossman <maxgrossman@users.noreply.github.com>
2019-12-16 14:20:15 -0500

    create class, child class for combobox (#60)
    * decouple combobox's core functions from tag related functions
    ref #59
    
    * fix the filter out logic
    
    ref #59
    
    * implement filter in child comboboxes
    
    ref #59
    
    * npm audit auto-fix
    ref #59

--- src/app/modules/combobox.module.ts
+++ src/app/modules/combobox.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { ComboboxComponent } from '../shared/components/combobox/combobox.component';
+import { TagComboboxComponent } from '../edit-maprule/tag-combobox/tag-combobox.component'
 import { ComboboxPipe } from '../shared/components/combobox/combobox.pipe';
 
 @NgModule({
   imports:      [ BrowserModule, ReactiveFormsModule ],
-  declarations: [ ComboboxComponent, ComboboxPipe ],
-  exports: [ ComboboxComponent ]
+  declarations: [ ComboboxComponent, TagComboboxComponent, ComboboxPipe ],
+  exports: [ ComboboxComponent, TagComboboxComponent ]
 })
 export class ComboboxModule { }
